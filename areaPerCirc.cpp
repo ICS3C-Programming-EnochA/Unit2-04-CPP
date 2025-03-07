d39452ffe318eff9c6454be03c3d89b10909bba3
Enoch Amedjrovi <enoch.amedjrovi@stu.ocsb.ca>
2025-03-07 22:48:26 +0000

    areaPerCirc.cpp

---
+++ areaPerCirc.cpp
@@ -0,0 +1,23 @@
+// Copyright (c) 2025 Enoch Amedjrovi All rights reserved
+// Created by:Enoch Amedjrovi
+// 7 March 7, 2025
+// This program calculates the area and perimeter of a circle.
+
+#include <cmath>
+#include <iostream>
+
+int main() {
+    // Radius
+    double rad = 10;
+
+    // Process
+    int circ = M_PI * (2 * rad);
+    int area = M_PI * (rad * rad);
+
+    // Output
+    std::cout << circ << " " << area << std::endl;
+    std::cout << sqrt(100) << std::endl;
+    std::cout << sqrt(25) << std::endl;
+
+    return 0;
+}
